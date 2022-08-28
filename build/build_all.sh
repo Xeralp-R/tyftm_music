d14f480e6377c1638328dcd67fb413f17f93a63b
Xer-Alp <80310657+Xeralp-R@users.noreply.github.com>
2022-08-29 07:18:44 +0800

    Renamed export to build
    created new export folder just for the pdfs and things

---
+++ build/build_all.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+/usr/local/bin/lilypond --output=/users/mariquitreventar/Desktop/Acodes/Music/Mass1/exports/ /users/mariquitreventar/Desktop/Acodes/Music/Mass1/scores/Full_Score.ly
+/usr/local/bin/lilypond --output=/users/mariquitreventar/Desktop/Acodes/Music/Mass1/exports/ /users/mariquitreventar/Desktop/Acodes/Music/Mass1/scores/Part_Score_Piano.ly
+/usr/local/bin/lilypond --output=/users/mariquitreventar/Desktop/Acodes/Music/Mass1/exports/ /users/mariquitreventar/Desktop/Acodes/Music/Mass1/scores/Part_Score_Asax.ly
+/usr/local/bin/lilypond --output=/users/mariquitreventar/Desktop/Acodes/Music/Mass1/exports/ /users/mariquitreventar/Desktop/Acodes/Music/Mass1/scores/Part_Score_Violin_1.ly
+/usr/local/bin/lilypond --output=/users/mariquitreventar/Desktop/Acodes/Music/Mass1/exports/ /users/mariquitreventar/Desktop/Acodes/Music/Mass1/scores/Part_Score_Violin_2.ly
+/usr/local/bin/lilypond --output=/users/mariquitreventar/Desktop/Acodes/Music/Mass1/exports/ /users/mariquitreventar/Desktop/Acodes/Music/Mass1/scores/Part_Score_Viola.ly
+/usr/local/bin/lilypond --output=/users/mariquitreventar/Desktop/Acodes/Music/Mass1/exports/ /users/mariquitreventar/Desktop/Acodes/Music/Mass1/scores/Part_Score_Cello.ly
+/usr/local/bin/lilypond --output=/users/mariquitreventar/Desktop/Acodes/Music/Mass1/exports/ /users/mariquitreventar/Desktop/Acodes/Music/Mass1/scores/Part_Score_Contrabass.ly
+
+/usr/local/bin/lilypond --output=/users/mariquitreventar/Desktop/Acodes/Music/Mass1/exports/ /users/mariquitreventar/Desktop/Acodes/Music/Mass1/scores/Choir_Score.ly
+/usr/local/bin/lilypond --output=/users/mariquitreventar/Desktop/Acodes/Music/Mass1/exports/ /users/mariquitreventar/Desktop/Acodes/Music/Mass1/scores/Part_Score_Cantor.ly
+/usr/local/bin/lilypond --output=/users/mariquitreventar/Desktop/Acodes/Music/Mass1/exports/ /users/mariquitreventar/Desktop/Acodes/Music/Mass1/scores/Part_Score_Soprano_Tutti.ly
+/usr/local/bin/lilypond --output=/users/mariquitreventar/Desktop/Acodes/Music/Mass1/exports/ /users/mariquitreventar/Desktop/Acodes/Music/Mass1/scores/Part_Score_Soprano_Solo.ly
+/usr/local/bin/lilypond --output=/users/mariquitreventar/Desktop/Acodes/Music/Mass1/exports/ /users/mariquitreventar/Desktop/Acodes/Music/Mass1/scores/Part_Score_Alto_Tutti.ly
+/usr/local/bin/lilypond --output=/users/mariquitreventar/Desktop/Acodes/Music/Mass1/exports/ /users/mariquitreventar/Desktop/Acodes/Music/Mass1/scores/Part_Score_Tenor_Tutti.ly
+/usr/local/bin/lilypond --output=/users/mariquitreventar/Desktop/Acodes/Music/Mass1/exports/ /users/mariquitreventar/Desktop/Acodes/Music/Mass1/scores/Part_Score_Bass_Tutti.ly
+/usr/local/bin/lilypond --output=/users/mariquitreventar/Desktop/Acodes/Music/Mass1/exports/ /users/mariquitreventar/Desktop/Acodes/Music/Mass1/scores/Part_Score_Bass_Solo.ly
