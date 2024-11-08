bf3edc5139f0e9b4263d74c25ddafc71854b3284
Xer-Alp <80310657+Xeralp-R@users.noreply.github.com>
2024-11-08 08:06:16 +0800

    Cleaned for TYFTM

--- build/build_repertoire_1.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-/usr/local/bin/lilypond --output=/users/mariquitreventar/Desktop/Acodes/Music/Mass1/exports/ /users/mariquitreventar/Desktop/Acodes/Music/White_Book/src/First_Reportoire/First_Repertoire.ly
