9ef21f1b20ea5959ec83f2ebc68b13a176380387
Elaiah Asperin <esasperin@gmail.com>
2025-01-27 20:53:24 +0800

    Added delete, add, and replace commands

--- automations/main.py
+++ automations/main.py
@@ -1,9 +1,95 @@
 # Main script for running revision automations
 
-import csv
+import fileinput
 
-with open("automations/revisions.csv", newline='') as csvfile:
-    reader = csv.reader(csvfile)
-    writer = csv.writer(csvfile)
-    for row in reader:
-        print(', '.join(row))
+import modules.file_manager as fm
+import modules.commands as cmd
+
+tasks = fm.readRevisions()
+
+for score in tasks:
+    for instrument in tasks[score]:
+        if instrument in fm.FILES.keys() and instrument in fm.VARIABLES.keys():
+            file = fm.FILES[instrument]
+            variable = fm.VARIABLES[instrument]
+        else:
+            continue
+
+        commands = tasks[score][instrument]
+        in_variable = False
+        bar_no = 1
+
+        for line in fileinput.input(f"sources/{score}/{file}.ly", inplace=True):
+            if in_variable:
+                if '}' in line:
+                    in_variable = False
+                    print(line, end='')
+                    continue
+
+                new_line = line
+                tokens = line.split(' ')
+
+                for command in commands:
+                    if command['bar'] == bar_no:
+                        if command['command'][0] == 'del':
+                            new_line = cmd.delete(line, command['command'][1:])
+                        elif command['command'][0] == 'add':
+                            new_line = cmd.add(line, *command['command'][1:])
+                        elif command['command'][0] == 'rep':
+                            new_line = cmd.replace(line, *command['command'][1:])
+                        elif command['command'][0] == 'move':
+                            new_line = cmd.move(line, *command['command'][1:])
+
+                print(new_line, end='')
+
+                if len(tokens) >= 2:
+                    if tokens[-2] == '%' or '#' in tokens[-1]:
+                        bar_no = int(tokens[-1])
+
+            elif variable in line:
+                in_variable = True
+                print(line, end='')
+            else:
+                print(line, end='')
+
+# # Modifies each score according to the commands in the revision sheet
+# for score in scores:
+#     folder = '_'.join(score.split()[1:], ).lower()
+#     fm = {}
+
+#     for instrument in directory[score]:
+#         if instrument in INSTRUMENT_FILES.keys() and instrument in INSTRUMENT_VARIABLES.keys():
+#             file = INSTRUMENT_FILES[instrument]
+#             variable = INSTRUMENT_VARIABLES[instrument]
+#             lines = list(filter(lambda x: instrument in x, sheet))
+#             bars = [line[2] for line in lines]
+#             commands = [line[3].split('; ') for line in lines]
+#             fm[instrument] = 
+
+# # Modifies each instrument according to the commands in the revision sheet
+# for instrument in directory:
+#     if instrument in INSTRUMENT_FILES.keys():
+#         with open(f"sources/{score}/{INSTRUMENT_FILES[instrument]}.ly") as source:
+#             pass
+#     else:
+#         print(f"Instrument {instrument} not registered. Proceeding to the next instrument...")
+
+# # Processes requests in revisions sheet
+# for line in sheet:
+#     score = '_'.join(line[0].split()[1:], ).lower()
+#     instrument = line[1]
+#     bar = line[2]
+#     instructions = [x.split(' ') for x in line[3].split('; ')]
+
+#     if instrument in INSTRUMENT_FILES.keys():
+#         with open(f"sources/{score}/{INSTRUMENT_FILES[instrument]}.ly") as source:
+#             for instruction in instructions:
+#                 command = instruction[0]
+#                 arguments = instruction[1:]
+
+#                 if command == "del":
+#                     pass
+
+#     else:
+#         notImplemented()
+      
---
+++ automations/modules/commands.py
@@ -0,0 +1,137 @@
+CATEGORIES = {
+    'dyn': ['\\p', '\\m', '\\f'],
+    'tie': ['tie'],
+    'note': ['a', 'b', 'c', 'd', 'e', 'f', 'g'],
+    'rest': ['R'],
+}
+
+POSITIONS = {
+    'default': '_',
+    'sb': '_',
+}
+
+# Categorizes graphics
+def __categorize(graphic):
+    for category in CATEGORIES:
+        for determiner in CATEGORIES[category]:
+            if determiner in graphic:
+                return category
+    
+    return "Uncategorized"
+
+# Deletes all instances of a certain graphic in a certain bar
+def delete(line: str, item: str, pos: str = None):
+    new_line = line
+
+    # For categorical deletions (e.g., deleting all dynamics)
+    if not pos and item in CATEGORIES:
+        tokens = line.split()
+        graphics = CATEGORIES[item]
+        for i in range(len(tokens)):
+            for graphic in graphics:
+                if graphic in tokens[i]:
+                    tokens[i] = ''
+
+        new_line = ' '.join(list(filter(bool, tokens)))
+    
+    # For specific deletions
+    else:
+        new_line = new_line.replace(f"{POSITIONS[pos]}{item}", '')
+
+    return new_line
+
+# Add a new graphic at a certain position in the score
+def add(line: str, graphic: str, note_no: int = 1, pos: str = 'default'):
+    tokens = line.split(' ')
+    category = __categorize(graphic)
+    note_count = 0
+    i = 0
+
+    # Assuming new notes will be added onto empty bars
+    if category == 'note' and __categorize(tokens[0]) == 'rest':
+        if tokens[-2] == '%' or '#' in tokens[-1]:
+            bar_no = int(tokens[-1])
+
+        rest = tokens[0].split('*')
+        note_len = 1 / int(graphic[1:])
+        rest_len = 1 - note_len
+
+        if len(rest) == 1:
+            if (1 / rest_len).is_integer():
+                rest_no = int(1 / rest_len)
+                tokens[0] = f"R{rest_no}"
+                tokens.insert(0, graphic)
+        else:
+            new_rest_count = int(rest[1]) - 1
+            tokens[0] = f"R1*{new_rest_count}"
+
+            tokens.insert(0, '\n')
+            tokens.insert(0, str(bar_no - new_rest_count))
+            tokens.insert(0, '%')
+            tokens.insert(0, '|')
+
+            if (1 / rest_len).is_integer():
+                rest_no = int(1 / rest_len)
+                tokens.insert(0, f"R{rest_no}")
+                tokens.insert(0, graphic)
+    else:
+        for token in tokens:
+            if __categorize(token) == "note":
+                note_count += 1
+                if note_count == note_no:
+                    if category == 'tie':
+                        tokens[i] += '~'
+                    elif category == 'dyn':
+                        tokens.insert(i + 1, f"{POSITIONS[pos]}{graphic}")
+                    
+                    # Add commands only add one graphic at a time, so there
+                    # is no need to keep running once one graphic has been added
+                    break
+            
+            i += 1
+
+    return ' '.join(tokens)
+
+# Replace one graphic with another
+def replace(line: str, graphic_orig: str, graphic_new: str):
+    tokens = line.split(' ')
+
+    for i in range(len(tokens)):
+        token = tokens[i]
+        if graphic_orig in token:
+            tokens[i] = token.replace(graphic_orig, graphic_new)
+
+    return ' '.join(tokens)
+
+# Moves graphics from one note to another
+def move(line: str, graphic: str, note_no_orig: int, note_no_new: int):
+    raise NotImplementedError()
+
+# Moves graphics from one position to another
+def move(line: str, graphic: str, note_no: int, pos_orig: str, pos_new: int):
+    tokens = line.split(' ')
+    category = __categorize(graphic)
+    note_count = 0
+    i = 0
+
+    for token in tokens:
+        if __categorize(token) == "note":
+            note_count += 1
+            if note_count == note_no:
+                if category == 'tie':
+                    tokens[i] += '~'
+                elif category == 'dyn':
+                    tokens.insert(i + 1, f"{POSITIONS[pos_new]}{graphic}")
+                
+                # Add commands only add one graphic at a time, so there
+                # is no need to keep running once one graphic has been added
+                break
+        
+        i += 1
+
+    return ' '.join(tokens)
+
+if __name__ == '__main__':
+    line = "R1*10 | % 12"
+    new_line = add(line, "g2")
+    print(new_line)
