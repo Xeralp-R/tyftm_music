ee207f323e4733b6fb947cbe691988f272b08a39
Meds Pogi <69465320+MedsAsperin@users.noreply.github.com>
2025-03-19 21:32:35 +0800

    Restructure automations directory

---
+++ automations/lysplitter/variables.py
@@ -0,0 +1,115 @@
+import ly.document
+
+def itemize(doc: ly.document.Document):
+    """Lists all variables in a Lilypond file
+
+    Args:
+        doc (ly.document.Document): _Lilypond document to scan_
+
+    Returns:
+        list: _names of all variables in doc_
+    """
+    runner = ly.document.Runner(doc)
+    variables = []
+
+    while token := runner.next():
+        if type(token) == ly.lex.lilypond.Name:
+            variables.append(token)
+
+    return variables
+
+def position(doc: ly.document.Document, var: str):
+    """Gives the position of a variable in a lilypond file
+
+    Args:
+        doc (ly.document.Document): _Lilypond document to search_
+        var (str): _name of variable to search_
+
+    Returns:
+        int: _position of variable in file, error if not found_
+    """
+    runner = ly.document.Runner(doc)
+
+    while token := runner.next():
+        if type(token) == ly.lex.lilypond.Name and token == var:
+            return runner.position()
+    else:
+        raise NameError("This variable does not exist in this document!")
+
+def extract(doc: ly.document.Document, var: str):
+    """Extracts a variable declaration from a Lilypond document into plain text
+
+    Args:
+        doc (ly.document.Document): _Lilypond document to extract variable from_
+        var (str): _name of variable to extract_
+    
+    Returns:
+        _ly.document.Document_: _Lilypond document containing variable declaration_
+    """
+    pos = position(doc, var)
+    runner = ly.document.Runner(doc)
+    runner.set_position(pos)
+
+    declaration = ""
+    bracket_found = False
+    level = 0
+
+    while token := runner.next():
+        declaration += token
+
+        if token == '{':
+            level += 1
+            bracket_found = True
+        elif token == '}':
+            level -= 1
+
+        # If parser is at the outermost layer and has passed a variable
+        # declaration, terminate
+        if level == 0 and bracket_found:
+            break
+    
+    return ly.document.Document(declaration)
+
+def delete(doc: ly.document.Document, var: str):
+    """Deletes a variable declaration from a Lilypond document
+
+    Args:
+        doc (ly.document.Document): _Lilypond document to delete variable from_
+        var (str): _name of variable to delete_
+    
+    Returns:
+        _ly.document.Runner_: _new Lilypond document without the specified variable_
+    """
+    runner = ly.document.Runner(doc)
+
+    new = ""
+    var_found = False
+    bracket_found = False
+    level = 0
+
+    while token := runner.next():
+        if var_found:
+            if token == '{':
+                level += 1
+                bracket_found = True
+            elif token == '}':
+                level -= 1
+
+            # If parser is at the outermost layer and has passed a variable
+            # declaration, terminate
+            if level == 0 and bracket_found:
+                var_found = False
+                new = new.strip("\n")
+        elif token == var:
+            var_found = True
+        else:
+            new += token
+    
+    return ly.document.Document(new)
+
+if __name__ == "__main__":
+    doc = ly.document.Document.load("sources/Scene Change 4a.ly")
+    # print(itemize("sources/Scene Change 4a.ly"))
+    # print(position(doc, "part-Pone-one"))
+    # print(extract(doc, "part-Ptwo-one").plaintext())
+    print(delete(doc, "part-Ptwo-one").plaintext())
---
+++ automations/lysplitter/wrappers.py
@@ -0,0 +1,57 @@
+from pathlib import Path
+import ly.document
+import variables
+import files
+
+def itemize(piece: str):
+    """Wrapper function for `variables.itemize()`: prints space-separated
+    listing all variable names instead of simply returning a list, 0 if
+    unsuccessful
+
+    Args:
+        piece (str): _path of file to scan for variables_
+    """
+    try:
+        if not Path(piece).is_file():
+            raise FileNotFoundError("This piece does not exist!")
+        else:
+            doc = ly.document.Document.load(piece)
+            out = variables.itemize(doc)
+            print(' '.join(out))
+    except Exception as ex:
+        print(0, str(type(ex)))
+
+def separate(piece: str, var: str, inst: str):
+    """Wrapper function that follows the algorithm below:
+    1. Parse piece into a Lilypond document
+    2. Extract specified variable from document
+    3. Engrave extracted variable declaration onto new file
+    4. Remove variable from document
+    5. Add include command for new file containing variable declaration
+    6. Engrave new version of document onto original piece file
+    7. Print 1 if successful, 0 otherwise
+
+    Args:
+        piece (str): _path of file to process; **enter filepaths using
+        backslashes, not forward slashes**_
+        var (str): _name of variable to extract and engrave onto new file_
+        inst (str): _name of new file (instrument)_
+    """
+    try:
+        if not Path(piece).is_file():
+            raise FileNotFoundError("This piece does not exist!")
+        else:
+            doc = ly.document.Document.load(piece)
+            declaration = variables.extract(doc, var)
+            folder_path = f".\\sources\\{piece.split('\\')[-1].replace('.ly', '')}"
+            Path(folder_path).mkdir(parents=True, exist_ok=True)
+            files.engrave(f"{folder_path}\\{inst}.ly", declaration)
+            new_doc = variables.delete(files.include(piece, inst), var)
+            files.engrave(piece, new_doc, overwrite=True)
+            print(1)
+    except Exception as ex:
+        print(0, str(type(ex)))
+
+if __name__ == "__main__":
+    # itemize(".\\sources\\Scene Change 4a.ly")
+    separate(".\\sources\\Scene Change 4a.ly", "part-Pone-one", "test")
